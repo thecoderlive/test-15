f0c460858346350c30a2190574c8a5d6efcbd9aa
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 14:11:16 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "51278e3e-fe9e-46fe-ad77-7cef8c741c7d",
+    "home_feed": [
+        {
+            "id": "ce309aab-7c75-4cc6-84d8-1fc7ed5ae0de",
+            "artist_name": "study",
+            "home_carousel": [
+                {
+                    "id": "9f6967da-515b-47ac-a501-485f06868596",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png"
+                },
+                {
+                    "id": "dec05cdb-dd4b-473c-9a3a-6a8ae923a962",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "cf44023f-6586-430d-ae99-2e33162f9a82",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "3a02fa4c-105f-4266-88f1-f941bbd3a42f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "93320bda-cda8-43dd-8391-8058e1d5a638",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                }
+            ],
+            "art_caption": "west",
+            "art_description": "Right tree fire early drug theory. Response parent section type kind. Yourself develop example about fact."
+        },
+        {
+            "id": "769e8ec1-5f83-4c1f-bd07-4d7ffb46283e",
+            "artist_name": "office",
+            "home_carousel": [
+                {
+                    "id": "4aff08e0-4f65-4e18-8d88-99e53cc1bd6a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                },
+                {
+                    "id": "c6248c54-2b0d-487c-8008-66c6dd528fc6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "7a7dcabc-921e-4608-bc86-66690e0f34d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                },
+                {
+                    "id": "408eae51-2ab2-4823-b76a-d024839104de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png"
+                },
+                {
+                    "id": "7515813a-0f0c-4c49-bbae-b47802293176",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png"
+                }
+            ],
+            "art_caption": "present",
+            "art_description": "Key material land black us. These particularly court. System experience magazine sea respond newspaper beat."
+        },
+        {
+            "id": "3e63d574-98ea-4dc1-9c72-bb317b0a74eb",
+            "artist_name": "drive",
+            "home_carousel": [
+                {
+                    "id": "765c335f-91a4-40ca-baa9-145bd476faad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "dfc9dfe0-be7e-4b99-b001-a456b6bf8d4a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "fbf73b69-846f-4666-9453-44e6c886d25b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "2da6ae53-0159-4838-98c5-7624addcd59f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/227.png"
+                },
+                {
+                    "id": "26c6da20-44e9-4dd2-b8f7-eb9f82fe36f0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                }
+            ],
+            "art_caption": "young",
+            "art_description": "Develop argue film age poor eight gas. Picture sing job give."
+        },
+        {
+            "id": "60ad632f-d3ee-4b25-989f-e59ea9bb9493",
+            "artist_name": "piece",
+            "home_carousel": [
+                {
+                    "id": "204e7cf5-7bbd-4cb7-bb4a-20f4c03a1da2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png"
+                },
+                {
+                    "id": "2fd76678-4aa3-4904-9633-5c7b8977abdf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                },
+                {
+                    "id": "cb47dd7e-7664-4007-aa80-9360923be1d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "4f0dd5c9-6a44-4e98-afdd-9ff28f85df71",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png"
+                },
+                {
+                    "id": "13568e85-6491-4e58-ab66-0791a00be69a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                }
+            ],
+            "art_caption": "treatment",
+            "art_description": "Center top him kid social draw involve. Because last draw soldier wonder positive. Building item organization edge tough friend."
+        },
+        {
+            "id": "33ca850f-e64f-49f3-9064-466380a4330d",
+            "artist_name": "parent",
+            "home_carousel": [
+                {
+                    "id": "18b046a0-752b-4524-9172-cd837446c66d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "e8401415-950e-4ed6-8feb-62693dec9624",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "d01ba22f-9805-4a5c-9578-58e167e4b182",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png"
+                },
+                {
+                    "id": "9feb5cba-7929-4595-b8f6-e268c734e22a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                },
+                {
+                    "id": "be0ea741-4a31-4d51-9083-f9d207393acd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                }
+            ],
+            "art_caption": "maintain",
+            "art_description": "Street develop remember boy share others. Well mean or visit blood seem while pretty. Eye increase population become."
+        }
+    ]
+}
